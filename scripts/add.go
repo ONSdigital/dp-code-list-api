b36b2cdd42c9974fbea2ffc36c2bf075e0ed6782
Eleanor Deal <eleanor.deal@gmail.com>
2017-10-20 14:59:11 +0100

    Add script to convert CSV to JSON for mongo import

---
+++ scripts/add.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/ONSdigital/dp-code-list-api/models"
+	"github.com/google/uuid"
+)
+
+// Code for a single dimensions type
+type Code struct {
+	ID    string           `json:"id"`
+	Code  string           `json:"code"    bson:"code"`
+	Label string           `json:"label"   bson:"label"`
+	Links models.CodeLinks `json:"links"   bson:"links"`
+}
+
+func main() {
+
+	//args - input file, output code file
+	input := flag.String("input", "", "input file name")
+
+	flag.Parse()
+
+	if len(*input) == 0 {
+		log.Fatal("-input argument required")
+	}
+
+	f, _ := os.Open(*input)
+	csvr := csv.NewReader(f)
+	defer f.Close()
+
+	recs, err := csvr.ReadAll()
+	if err != nil {
+		log.Fatal("Failed to read csv file", err)
+		os.Exit(0)
+	}
+
+	header := recs[0]
+	recs = recs[1:]
+
+	listID := createCodeList(header)
+
+	createCodes(recs, listID)
+
+	log.Print("Successfully added rows to both files")
+	os.Exit(1)
+}
+
+func createCodes(recs [][]string, listID string) {
+	filename := "codes/" + listID + ".json"
+
+	if err := os.Rename(filename, filename+".old"); err != nil {
+		log.Fatal("File already exists and cannot be renamed", err)
+	}
+
+	var wg sync.WaitGroup
+	for _, v := range recs {
+		id := uuid.New().String()
+		code := Code{
+			ID:    id,
+			Code:  v[0],
+			Label: v[1],
+			Links: models.CodeLinks{
+				CodeList: models.Link{
+					ID:   listID,
+					Href: "http://localhost:22400/code-lists/" + listID,
+				},
+				Self: models.Link{
+					ID:   v[0],
+					Href: "http://localhost:22400/code-lists/" + listID + "/codes/" + v[0],
+				},
+			},
+		}
+
+		wg.Add(1)
+		go func(code Code) {
+			b, err := json.Marshal(code)
+			if err != nil {
+				log.Fatal("cannot marshal json for codes", err)
+			}
+
+			if err := appendToFile(b, filename); err != nil {
+				log.Fatal("failed to append code-list to file", err)
+			}
+			wg.Done()
+		}(code)
+	}
+	wg.Wait()
+
+	imp := "mongoimport --db codelists --collection codes --file $scriptDir/" + filename
+
+	if err := appendToFile([]byte(imp), "setup.sh"); err != nil {
+		log.Fatal("failed to append code file to setup script", err)
+	}
+}
+
+func createCodeList(header []string) string {
+	listID := uuid.New().String()
+
+	if len(header[0]) > 0 {
+		listID = header[0]
+	}
+
+	cl := models.CodeList{
+		ID:   listID,
+		Name: header[1],
+		Links: models.CodeListLink{
+			Self: models.Link{
+				ID:   listID,
+				Href: "http://localhost:22400/code-lists/" + listID,
+			},
+			Codes: models.Link{
+				Href: "http://localhost:22400/code-lists/" + listID + "/codes",
+			},
+		},
+	}
+
+	b, err := json.Marshal(cl)
+	if err != nil {
+		log.Fatal("cannot marshal json for code-list line", err)
+	}
+
+	if err := appendToFile(b, "codelists.json"); err != nil {
+		log.Fatal("failed to append code-list to file", err)
+	}
+
+	return listID
+}
+
+func appendToFile(b []byte, filename string) error {
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	s := string(b) + "\n"
+
+	if _, err := f.WriteString(s); err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,10 +2,12 @@
 
 mongo mongodb://localhost:27017/codelists <<EOF
 db.dropDatabase();
+db.codelists.ensureIndex({"id":1},{"background":true});
 db.codes.ensureIndex({"links.code_list.id":1},{"background":true});
 db.codes.ensureIndex({"code":1,"links.code_list.id":1},{"background":true});
 EOF
 
 scriptDir=$( dirname "${BASH_SOURCE[0]}" )
 mongoimport --db codelists --collection codelists --file $scriptDir/codelists.json
-mongoimport --db codelists --collection codes --file $scriptDir/codes.json
+mongoimport --db codelists --collection codes --file $scriptDir/time.json
+mongoimport --db codelists --collection codes --file $scriptDir/codes/cpi1dim1aggid.json

9c228c2da656ac271c6db1e39ddab7d0ec8aad7d
robchamberspfc <robchamberspfc@gmail.com>
2017-11-30 16:57:21 +0000

    remove additional codelists not required for beta

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,7 +19,5 @@ import_to() {
 import_to codelists codelists.json
 import_to codes codes/time.json
 import_to codes codes/cpi1dim1aggid.json
-import_to codes codes/54ff5089-ea78-45ef-afa2-0dfe58f89497.json
-import_to codes codes/4e1a21a9-3fb9-4a71-b2ad-9be29457236b.json
 import_to codes codes/cpih1dim1aggid.json
 import_to codes codes/uk-only.json

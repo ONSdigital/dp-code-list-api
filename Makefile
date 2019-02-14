18b0941113ce96308ba18ce58851bfb53879a5bd
Eleanor Deal <eleanor.deal@gmail.com>
2019-02-14 15:55:37 +0000

    Update dp-graph dependency for config var name change

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ test:
 	go test -cover $(shell go list ./... | grep -v /vendor/)
 
 debug:
-	HUMAN_LOG=1 GRAPH_DRIVER=neo4j GRAPH_ADDR="bolt://localhost:7687" go run -race cmd/dp-code-list-api/main.go
+	HUMAN_LOG=1 GRAPH_DRIVER_TYPE=neo4j GRAPH_ADDR="bolt://localhost:7687" go run -race cmd/dp-code-list-api/main.go
 
 acceptance:
 	HUMAN_LOG=1 NEO4J_CODE_LIST_LABEL=code_list_acceptance go run -race cmd/dp-code-list-api/main.go
--- vendor/github.com/ONSdigital/dp-graph/mock/instance.go
+++ vendor/github.com/ONSdigital/dp-graph/mock/instance.go
@@ -2,22 +2,8 @@ package mock
 
 import (
 	"context"
-	"errors"
 )
 
 func (m *Mock) CountInsertedObservations(ctx context.Context, instanceID string) (count int64, err error) {
-	if m.IsBackendReachable != true {
-		return 0, errors.New("database unavailble - 500")
-	}
-
-	if m.IsQueryValid != true {
-		return 0, errors.New("invalid query - 400")
-	}
-
-	if m.IsContentFound != true {
-		return 0, errors.New("not found - 404")
-	}
-
-	return 100, nil
-
+	return 0, m.checkForErrors()
 }

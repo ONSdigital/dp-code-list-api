ef3f0cd48572b3b727b0b531b445b3d43e0db102
Eleanor Deal <eleanor.deal@gmail.com>
2019-07-11 14:51:01 +0100

    Update vendor with dp-graph neptune implementations for code lists

---
+++ vendor/github.com/ONSdigital/dp-graph/neptune/driver/neptunepool.go
@@ -0,0 +1,20 @@
+package driver
+
+import (
+	gremgo "github.com/gedge/gremgo-neptune"
+)
+
+//go:generate moq -out ../internal/pool.go -pkg internal . NeptunePool
+
+/*
+NeptunePool defines the contract required of the gremgo
+connection Pool by the Neptune.Driver.
+*/
+type NeptunePool interface {
+	Close()
+	Execute(query string, bindings, rebindings map[string]string) (resp []gremgo.Response, err error)
+	Get(query string, bindings, rebindings map[string]string) (resp interface{}, err error)
+	GetCount(q string, bindings, rebindings map[string]string) (i int64, err error)
+	GetE(q string, bindings, rebindings map[string]string) (resp interface{}, err error)
+	GetStringList(query string, bindings, rebindings map[string]string) (vals []string, err error)
+}
---
+++ vendor/github.com/gedge/graphson/go.mod
@@ -0,0 +1,3 @@
+module github.com/gedge/graphson
+
+go 1.12

4b40c2ca49492ed819fa895d0c3098a756fb286a
red <red54321@outlook.com>
2020-10-09 08:36:47 +0100

    doc. explain the existence of 'Offset'
    Add comments to explain the intended use of the Offset struct member.

--- models/codelist.go
+++ models/codelist.go
@@ -9,11 +9,26 @@ import (
 
 // CodeListResults contains an array of code lists which can be paginated
 type CodeListResults struct {
-	Items      []CodeList `json:"items"`
-	Count      int        `json:"count"`
-	Offset     int        `json:"offset"`
-	Limit      int        `json:"limit"`
-	TotalCount int        `json:"total_count"`
+	Items []CodeList `json:"items"`
+	Count int        `json:"count"`
+
+	// Offset is a TODO for future use. Its use will be to do with pagination.
+	// It is intended to be used with the other structure members where:
+	//   Limit - max number of items we're returning in this response
+	//           (e.g. 20, or 50, rather than all we use atm)
+	//   Count - how many items are actually present in the response
+	//   TotalCount - how many total items there may be
+	//                (so the full list size, maybe thousands)
+	//   Offset - the number of documents into the full list that this
+	//            particular response is starting at.
+	//
+	// So in a list that has a totalCount of 511, we might set a limit of 100,
+	// an offset of 500, and get a response whose count is 11, because its the
+	// last 11 documents in the list.
+	Offset int `json:"offset"`
+
+	Limit      int `json:"limit"`
+	TotalCount int `json:"total_count"`
 }
 
 // CodeList containing links to all possible codes
